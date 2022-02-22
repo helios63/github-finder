596d78a226b395eb285f8328d5fe05b344e6fa4e
Maxime Mondet <maxime.mondet@gmail.com>
2022-02-22 17:11:35 +0100

    Footer Added

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar({title}) {
             <div className="flex-none px-2 mx-2">
                 <FaGithub className="inline pr-2 text-3xl"/>
                 <Link to='/' className="text-lg font-bold align-middle">
-                {title}
+                    {title}
                 </Link>
             </div>
 
