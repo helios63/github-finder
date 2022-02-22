fc6a1e3e2a695b4bf322d6685043103a7aa19d62
Maxime Mondet <maxime.mondet@gmail.com>
2022-02-22 14:17:28 +0100

    Webpacks added :  react-router react-router-dom react-icons + Navbar component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Navbar from "./components/layout/Navbar";
+
 function App() {
   return (
-    <div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+      </div>
       <h1>Hello world!</h1>
-    </div>
+    </Router>
   );
 }
 
